6d6f60b45800331f7dcc62aa1a4465fbfb84d6c5
Soni Kumari <sonikm443@gmail.com>
2024-10-21 14:29:42 +0530

    Time: 0 ms (100%), Space: 48.9 MB (53.38%) - LeetHub

--- 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.js
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.js
@@ -3,19 +3,19 @@
  * @return {number}
  */
 var bitwiseComplement = function(n) {
-    if(n === 0) return 1;
-   let ans = 0;
-        let i = 0;
+      if(n === 0) return 1;
+   let marks = 0;
+   let i = 0;
 
         while(n){
             // let bit = n & 1;
             if(!(n & 1)){
-              ans = 1 * Math.pow(2, i) + ans;
+              marks = 1 * Math.pow(2, i) + marks;
             }
 
             n = n >> 1;
             i++;
         }
 
-        return ans; 
+        return marks; 
 };
