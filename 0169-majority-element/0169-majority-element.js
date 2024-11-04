628d47a33539b927a92caa3fabab28bec939264e
Soni Kumari <sonikm443@gmail.com>
2024-11-04 13:00:34 +0530

    Time: 4 ms (60.78%), Space: 52.9 MB (32.93%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    let n = nums.length;   
+   // Sorting
+   nums.sort((a,b) => a-b);
+
+   // find majority element
+   let freq = 1;
+   let ans = nums[0];
+
+   for(let i=1; i<n; i++ ){
+    if(nums[i] == nums[i-1]){
+         freq++;
+    }
+   
+    else {
+        freq = 1;
+        ans = nums[i];
+    }
+
+   if((freq) > Math.floor(n/2)) return ans;
+
+   }
+
+    return ans;
+};
