af063a6e0ef159d10f9660b69dd20eb155f63b97
Soni Kumari <sonikm443@gmail.com>
2024-12-06 00:58:42 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0443-string-compression/0443-string-compression.js
+++ 0443-string-compression/0443-string-compression.js
@@ -3,33 +3,63 @@
  * @return {number}
  */
 var compress = function(chars) {
-    let str = "";
-    let i=0, n = chars.length;
+    let n = chars.length;
+    let i=0;
     let freq=1;
 
-    for(let j=1; j<n; j++){
-         if(chars[j] == chars[j-1]) freq++;
-         else if((chars[j] != chars[j-1]) && (freq > 1) ){
-           str = str.concat(chars[j-1], freq);
-            // chars[i++] = `${freq}`;
-            freq = 1;
-         }
-         else {
-            str = str.concat(chars[j-1]);
-         }
+    // More otimazed
+    for(let j=1; j < n; j++){
+      if(chars[j] == chars[i]) freq++;
+      else if(chars[j] != chars[i] && freq > 1){
+          chars[i++] = chars[j-1];
+          for(let c of freq.toString()){
+            chars[i++] = c;
+          }
+          freq=1;
+      }
+      else {
+        chars[i++] = chars[j-1];
+      }
     }
 
     if(freq > 1){
-          str = str.concat(chars[n-1], freq);
+          chars[i++] = chars[n-1];
+          for(let c of freq.toString()){
+            chars[i++] = c;
+          }
     }else {
-          str = str.concat(chars[n-1]);
+         chars[i++] = chars[n-1];
     }
 
-    for(let i=0; i<str.length; i++){
-        chars[i] = `${str[i]}`;
-    }
+    return i;
 
-    console.log(str);
+     
 
-    return str.length;
+
+
+    // let str = "";
+    // let freq=1;
+
+    // for(let j=1; j<n; j++){
+    //      if(chars[j] == chars[j-1]) freq++;
+    //      else if(chars[j] != chars[j-1] && freq > 1){
+    //        str = str.concat(chars[j-1], freq);
+    //         freq = 1;
+    //      }
+    //      else {
+    //         str = str.concat(chars[j-1]);
+    //      }
+    // }
+
+    // if(freq > 1){
+    //       str = str.concat(chars[n-1], freq);
+    // }else {
+    //       str = str.concat(chars[n-1]);
+    // }
+
+    // for(let i=0; i<str.length; i++){
+    //     chars[i] = `${str[i]}`;
+    // }
+
+    // return str.length;
 };
