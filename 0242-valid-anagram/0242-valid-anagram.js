fa601d918f33aff34ec2486c6613b2ed02280d83
Soni Kumari <sonikm443@gmail.com>
2024-12-05 20:37:18 +0530

    Time: 66 ms (5.07%), Space: 56.4 MB (5.14%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    // s = s.split("").sort().join("");
+    // t = t.split("").sort().join("");
+
+    // return s === t;
+
+    //More optimized
+    if(s.length !== t.length) return false;
+
+    let obj1={}
+    let obj2={}
+
+    for(let i in s){
+        obj1[s[i]] = (obj1[s[i]] || 0) + 1;
+        obj2[t[i]] = (obj2[t[i]] || 0) + 1;
+    }
+
+    for(let key in obj1){
+        if(obj1[key] != obj2[key]) return false;
+    }
+
+    return true;
+};
