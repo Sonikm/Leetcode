6940d82730608a6bc27b9b525505b757530cefec
Soni Kumari <sonikm443@gmail.com>
2024-12-06 16:52:00 +0530

    Time: 7 ms (15.58%), Space: 50.4 MB (32.82%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.js
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -7,18 +7,15 @@ var longestCommonPrefix = function(strs) {
     let ansStr = strs[0];
 
     for(let i=1; i<strs.length; i++){
-        let s = "";
         let word = strs[i];
-
-        for(let j=0; j<Math.min(word.length, ansStr.length); j++){
+        let j=0;
+        for(j; j<Math.min(word.length, ansStr.length); j++){
             if(ansStr[j] != word[j]) {
                 break;
             }
-            s += word[j];
         }
+        ansStr = ansStr.slice(0, j);
 
-        if(s === "") return "";
-        ansStr = s;
     }
 
     return ansStr;
