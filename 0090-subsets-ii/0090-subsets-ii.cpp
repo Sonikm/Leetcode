ed290eb380364ce7b3524045b0397149bb768cbd
Soni Kumari <sonikm443@gmail.com>
2024-12-16 18:07:49 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+
+   void getAllSubsets(vector<int>& nums, vector<int>& ans,int i, vector<vector<int>>& allSubsets, set<vector<int>>& s ){
+     if(i == nums.size()){
+        
+        if(s.find(ans) == s.end()){
+          allSubsets.push_back({ans});
+        }
+        s.insert(ans);
+        return;
+     }
+
+     // includes
+      ans.push_back(nums[i]);
+      getAllSubsets(nums, ans, i+1, allSubsets, s);
+
+      //excludes
+      ans.pop_back();
+      getAllSubsets(nums, ans, i+1, allSubsets, s);
+   }
+    
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>> allSubsets;
+        set<vector<int>> s;
+        vector<int> ans;
+        getAllSubsets(nums, ans, 0, allSubsets, s);
+
+        return allSubsets;
+    }
+};
