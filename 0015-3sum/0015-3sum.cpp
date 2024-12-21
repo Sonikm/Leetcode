463d8653b66476f26d14bfa0bc7b303f33158106
Soni Kumari <sonikm443@gmail.com>
2024-12-21 11:51:13 +0530

    Time: 59 ms (36.82%), Space: 31.1 MB (29.39%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -9,6 +9,7 @@ public:
         int n = nums.size();
 
         for(int i=0; i<n; i++){
+            if(i>0 && nums[i] == nums[i-1]) continue;
             int j = i+1, k = n-1;
 
             while(j<k){
@@ -18,12 +19,14 @@ public:
                 else if(sum < 0) j++;
                 else {
                     vector<int> temp = {nums[i], nums[j], nums[k]};
-                    if(s.find(temp) == s.end()){
-                        ans.push_back(temp);
-                        s.insert(temp);
-                    }
+                    ans.push_back(temp);
+                    // if(s.find(temp) == s.end()){
+                    //     s.insert(temp);
+                    // }
                     j++, k--;
                 }
+
+                while(j > i+1 && nums[j] == nums[j-1] && j<k) j++;
             }
         }
      
