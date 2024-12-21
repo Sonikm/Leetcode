a7551b45f755b598a61ffff8c8ff15ba294bf2c9
Soni Kumari <sonikm443@gmail.com>
2024-12-21 11:34:45 +0530

    Time: 1344 ms (6.54%), Space: 295.4 MB (9.12%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,33 +1,103 @@
 class Solution {
 public:
+
     vector<vector<int>> threeSum(vector<int>& nums) {
-        
         vector<vector<int>> ans;
+        set<vector<int>>s;
+
+        sort(nums.begin(), nums.end());
         int n = nums.size();
 
-        sort(nums.begin(), nums.end()); // sort
-
         for(int i=0; i<n; i++){
-          
-          if(i>0 && nums[i] == nums[i-1]) continue;
-          int j=i+1, k = n-1;
+            int j = i+1, k = n-1;
 
-          while(j<k){
-            int sum = nums[i] + nums[j] + nums[k];
-            
-            if(sum > 0) k--;
-            else if(sum < 0) j++;
-            else {
-                vector<int> trip = {nums[i], nums[j], nums[k]};
-                ans.push_back(trip);
-                j++, k--;
+            while(j<k){
+                int sum = nums[i]+nums[j]+nums[k];
 
-                while(j<k && nums[j] == nums[j-1]) j++;
+                if(sum > 0) k--;
+                else if(sum < 0) j++;
+                else {
+                    vector<int> temp = {nums[i], nums[j], nums[k]};
+                    if(s.find(temp) == s.end()){
+                        ans.push_back(temp);
+                        s.insert(temp);
+                    }
+                    j++, k--;
+                }
             }
-          }
-       }
+        }
+     
+     return ans;
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+    //     vector<vector<int>> ans;
+    //     int n = nums.size();
 
-        return ans;
+    //     sort(nums.begin(), nums.end()); // sort
+
+    //     for(int i=0; i<n; i++){
+          
+    //       if(i>0 && nums[i] == nums[i-1]) continue;
+    //       int j=i+1, k = n-1;
+
+    //       while(j<k){
+    //         int sum = nums[i] + nums[j] + nums[k];
+            
+    //         if(sum > 0) k--;
+    //         else if(sum < 0) j++;
+    //         else {
+    //             vector<int> trip = {nums[i], nums[j], nums[k]};
+    //             ans.push_back(trip);
+    //             j++, k--;
+
+    //             while(j<k && nums[j] == nums[j-1]) j++;
+    //         }
+    //       }
+    //    }
+
+    //     return ans;
        
     }
 };
