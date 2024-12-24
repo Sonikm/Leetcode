768b49124574b9979bfadf51fd02c69b7a52b1e8
Soni Kumari <sonikm443@gmail.com>
2024-12-25 03:50:02 +0530

    Time: 0 ms (100%), Space: 59.3 MB (28.75%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -2,23 +2,42 @@ class Solution {
 public:
     int longestOnes(vector<int>& nums, int k) {
 
-     // OPTIMAL APPROACH
-      int i=0, j=0;
+    //  // MOST OPTIMAL APPROACH
+    //   int i=0, j=0;
       int n = nums.size();
 
-      while(j < n){
+    //   while(j < n){
         
-        if(nums[j] == 0) k--;
+    //     if(nums[j] == 0) k--;
 
-        // if k is negative that means we have used extra zeros so strink the size of sub array   
-        if(k < 0){
-            if(nums[i] == 0) k++; // if we are removing zero that means we need to increase the k 
-            i++;
+    //     // if k is negative that means we have used extra zeros so strink the size of sub array   
+    //     if(k < 0){
+    //         if(nums[i] == 0) k++; // if we are removing zero that means we need to increase the k 
+    //         i++;
+    //     }
+    //     j++;
+    //   } 
+
+    //   return j-i;
+     //  OPTIMAL APPROACH
+      int l=0, r=0;
+      int zeros = 0;
+      int ans = 0;
+
+      while(r < n){
+        
+        if(nums[r] == 0) zeros++;
+        
+        while(zeros > k){
+            if(nums[l] == 0) zeros--;
+            l++;
         }
-        j++;
+       
+        ans = max(ans, r-l+1);
+        r++;
       } 
 
-      return j-i;
+      return ans;
 
     // BRUTE FORCE APPROACH
     //    for(int i=0; i<n; i++){
