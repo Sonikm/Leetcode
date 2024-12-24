06c67af9cdb2f801cfe0969a0fba94afb4c2603e
Soni Kumari <sonikm443@gmail.com>
2024-12-25 00:43:02 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,21 +1,44 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int ans = 0;
+
+       // OPTIMAL APPROACH
+        int maxLen = 0;
         int n = s.size();
+        int l=0, r=0;
+        unordered_map<char, int> hash;
 
-        for(int i=0; i<n; i++){
-            unordered_map<char,int> hash;
-            for(int j=i; j<n; j++){
-              
-              int key = s[j]; // get index value
-              if(hash[key]) break;
+        while(r<n){
+           
+           // if it is present in our hash or not
+           if(hash[s[r]]){
+             // check if it is an the range or not
+              if(hash[s[r]] >= l){
+               l = hash[s[r]] + 1; // move to left 
+              }
+             maxLen = max(maxLen, r-l+1); // get max length
+           }
 
-              ans = max(ans, j-i+1);
-              hash[key] = 1; // mark visited
-            }
+           hash[s[r]] = r;
+           r++;
         }
 
-        return ans;
+        return maxLen;
+
+        // BRUTE FORCE APPROACH
+
+        // for(int i=0; i<n; i++){
+        //     unordered_map<char,int> hash;
+        //     for(int j=i; j<n; j++){
+              
+        //       int key = s[j]; // get index value
+        //       if(hash[key]) break;
+
+        //       ans = max(ans, j-i+1);
+        //       hash[key] = 1; // mark visited
+        //     }
+        // }
+
+        // return ans;
     }
 };
